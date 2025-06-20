d274fdd881b5276958058e3d22ae5584d9e574ba
Jeremie Drouet <jeremie.drouet@gmail.com>
2025-06-20 06:15:08 +0200

    feat: prepare base project
    Signed-off-by: Jeremie Drouet <jeremie.drouet@gmail.com>

---
+++ core/src/lib.rs
@@ -0,0 +1,32 @@
+use std::collections::HashSet;
+
+#[derive(Clone, Debug)]
+pub enum TagValueArray {
+    Text(Box<str>),
+    Integer(i64),
+}
+
+#[derive(Clone, Debug)]
+pub enum TagValue {
+    Text(Box<str>),
+    Integer(i64),
+    Array(TagValueArray),
+}
+
+#[derive(Clone, Debug)]
+pub struct Metric {
+    pub name: Box<str>,
+    pub timestamp: i64,
+    pub tags: HashSet<Box<str>, TagValue>,
+    pub value: MetricValue,
+}
+
+#[derive(Clone, Debug)]
+pub enum MetricValue {
+    Count(u64),
+    Gauge(f64),
+}
+
+pub trait Intake {
+    fn ingest(&self, values: &[Metric]) -> impl Future<Output = std::io::Result<()>> + Send;
+}
