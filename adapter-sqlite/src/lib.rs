9e1ca7e537d8b6e25aaca91afa1c90edfb85e138
Jeremie Drouet <jeremie.drouet@gmail.com>
2025-06-20 06:15:25 +0200

    feat: create sqlite adapter
    Signed-off-by: Jeremie Drouet <jeremie.drouet@gmail.com>

---
+++ adapter-sqlite/src/lib.rs
@@ -0,0 +1,34 @@
+#[derive(Clone, Debug)]
+pub struct SqliteConfig {
+    pub url: Option<String>,
+}
+
+impl SqliteConfig {
+    pub async fn build(&self) -> sqlx::Result<Sqlite> {
+        match self.url.as_deref() {
+            None | Some(":memory:") => sqlx::sqlite::SqlitePoolOptions::new()
+                .min_connections(1)
+                .max_connections(1)
+                .connect(":memory:")
+                .await
+                .map(Sqlite),
+            Some(other) => sqlx::sqlite::SqlitePoolOptions::new()
+                .connect_with(
+                    sqlx::sqlite::SqliteConnectOptions::new()
+                        .create_if_missing(true)
+                        .filename(other),
+                )
+                .await
+                .map(Sqlite),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Sqlite(sqlx::SqlitePool);
+
+impl AsRef<sqlx::SqlitePool> for Sqlite {
+    fn as_ref(&self) -> &sqlx::SqlitePool {
+        &self.0
+    }
+}
