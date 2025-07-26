ac1cfcc566ae976ac4264f364a1d70c0c00104b0
Jeremie Drouet <jeremie.drouet@gmail.com>
2025-07-26 15:16:56 +0200

    test: ensure sqlite can ingest events
    Signed-off-by: Jeremie Drouet <jeremie.drouet@gmail.com>

--- adapter/sqlite/src/event/intake.rs
+++ adapter/sqlite/src/event/intake.rs
@@ -16,3 +16,73 @@ impl Intake for Sqlite {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use myhomelab_event::EventSource;
+    use myhomelab_event::intake::Intake;
+
+    struct FakeEvent {
+        source: myhomelab_event::EventSource,
+        level: myhomelab_event::EventLevel,
+        message: &'static str,
+        timestamp: u64,
+        attributes: Option<serde_json::Value>,
+    }
+
+    impl myhomelab_event::intake::IntakeInput for FakeEvent {
+        type Attrs = serde_json::Value;
+
+        fn source(&self) -> &myhomelab_event::EventSource {
+            &self.source
+        }
+        fn level(&self) -> myhomelab_event::EventLevel {
+            self.level
+        }
+
+        fn message(&self) -> &str {
+            self.message
+        }
+
+        fn timestamp(&self) -> u64 {
+            self.timestamp
+        }
+
+        fn attributes(&self) -> Option<&Self::Attrs> {
+            self.attributes.as_ref()
+        }
+    }
+
+    #[tokio::test]
+    async fn should_insert_events() {
+        let sqlite = crate::SqliteConfig::default().build().await.unwrap();
+        sqlite.prepare().await.unwrap();
+
+        sqlite
+            .ingest(&FakeEvent {
+                source: EventSource::sensor("fake"),
+                level: myhomelab_event::EventLevel::Info,
+                message: "Hello World",
+                timestamp: 42,
+                attributes: Some(serde_json::json!({
+                    "foo": "bar",
+                })),
+            })
+            .await
+            .unwrap();
+
+        sqlite
+            .ingest(&FakeEvent {
+                source: EventSource::sensor("fake"),
+                level: myhomelab_event::EventLevel::Debug,
+                message: "Hello Debug World",
+                timestamp: 52,
+                attributes: Some(serde_json::json!({
+                    "foo": "bar",
+                    "baz": 42,
+                })),
+            })
+            .await
+            .unwrap();
+    }
+}
